7e5397e04a6be5814a6e2b6a22cf67cbd0bbb38a
brmayes <brmayes16@gmail.com>
2020-07-29 00:05:08 -0400

    Updates

--- src/Components/_metadata.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	title: "Brittany Renee Mayes",
-	baseurl: "brmayes.github.io",
-	email: "brittany.mayes@washpost.com",
-	profilePicture: "mayesb.jpg",
-	highlighter: "rouge",
-	markdown: "kramdown",
-};
